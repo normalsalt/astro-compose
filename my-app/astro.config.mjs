383480030f3d655363e7ec1bc87e503218dfb615
normalsalt <normalsalt@gmail.com>
2024-03-09 17:15:13 +0900

    Update my-app

--- my-app/astro.config.mjs
+++ my-app/astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://normalsalt.github.io',
-  base: '/astro-compose',
+	site: 'https://normalsalt.github.io',
+        base: '/astro-compose',
+	integrations: [mdx(), sitemap()],
 });
